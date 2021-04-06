5622c01e27ebbbcdf0616f755ca7e6014370299f
Danial J <dan90@live.com.my>
2021-04-06 15:48:16 +0800

    Update karel_loop.py

--- karel_loop.py
+++ karel_loop.py
@@ -43,3 +43,56 @@ def put_five_beepers() :
    for i in range(5):
    	put_beeper()
 ```
+
+### While Loops
+
+```
+# Uses a "while" loop to move Karel until it hits
+# a wall. Works on any sized world.
+from karel.stanfordkarel import *
+
+# the program starts with main
+def main():
+   # call the move to wall function
+   move_to_wall()
+
+# this is a very useful function 
+def move_to_wall():
+   # repeat the body while the condition holds
+   while front_is_clear():
+      move()
+```
+
+```
+# Uses a while loop to place a line of beepers.
+# This program works for a world of any size.
+# However, because each world requires one fewer
+# moves than put_beepers it always misses a beeper.
+from karel.stanfordkarel import *
+
+# program starts at main
+def main():
+   # repeats until karel faces a wall
+   while front_is_clear():
+      # place a beeper on current square
+      put_beeper()
+      # move to the next square
+      move()
+```
+
+```
+# Uses a while loop to place a line of beepers.
+# This program works for a world of any size.
+from karel.stanfordkarel import *
+
+# program starts at main
+def main():
+   # repeats until karel faces a wall
+   while front_is_clear():
+      # place a beeper on current square
+      put_beeper()
+      # move to the next square
+      move()
+   # solves the fencepost bug
+   put_beeper()
+```
